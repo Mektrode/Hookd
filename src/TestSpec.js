5da485bc9eaf8d77569ce074560d1bfcf7e00274
Mektro <mowz971@gmail.com>
2019-03-19 23:50:32 +0000

    First test (rndtoInt function)

--- src/TestSpec.js
+++ src/TestSpec.js
@@ -10,3 +10,9 @@
   => Check SqrRoot() & expect 1.732... (rounded to 2 with log of original & new value)
   => Check Sqr() & expect 9
 */
+
+describe("Testing the rndtoInt function", () => {
+    it("round float to nearest integer", () => {
+        expect(rndtoInt(8.33211)).toEqual(8);
+    });
+});
