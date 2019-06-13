e900f22a6f212b25163ad6cc14bca516760cee5b
Mektro <mowz971@gmail.com>
2019-06-13 21:23:31 +0100

    Makeovaaa

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>LETS GET READY TO RUMBLEEE</h1>
+    </div>
+  );
+}
+
+export default App;
