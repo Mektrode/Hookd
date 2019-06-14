f6e0586926704c40a596681351130503109f222f
Mektro <mowz971@gmail.com>
2019-06-14 22:58:07 +0100

    highscores comp mapping data

--- src/components/highscores.js
+++ src/components/highscores.js
@@ -5,13 +5,16 @@ export default function Test() {
   return (
     <div>
       <h3>Highscores:</h3>
-      <div className="scores-card">
-        <ul>
-          <li>Name : {highscores.scores[1].nameOfPlayer}</li>
-          <li>Accuracy : {highscores.scores[1].accuracy}%</li>
-          <li>Date : {highscores.scores[1].date}</li>
-        </ul>
-      </div>
+      {highscores.scores.map(score => (
+        <li>
+          <br />
+          Name : {score.nameOfPlayer}
+          <br />
+          Accuracy : {score.accuracy}%
+          <br />
+          Date : {score.date}
+        </li>
+      ))}
     </div>
   );
 }
