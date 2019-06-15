46f354b539c2cc63c69c644389732158c781db07
Mektro <mowz971@gmail.com>
2019-06-15 19:18:56 +0100

    Planning out minimal requirements for setup comp

--- src/components/setup.js
+++ src/components/setup.js
@@ -7,9 +7,23 @@ const PickName = () => {
       <h3>What would you like to be called?</h3>
       <input />
       <button>Next</button>
+      <br />
+      <br />
+      <button>Start as Guest</button>
     </div>
   );
 };
+
+/*
+Future feature to be able to choose which buttons the user would like to take to the challenge
+const PickButtons = () => {
+  return (
+    <div>
+      <h3>Choose your buttons wisely</h3>
+    </div>
+  );
+};
+*/
 
 export default function setup() {
   return (
