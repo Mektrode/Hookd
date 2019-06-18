288bcebf5786e07c8dce490d6f80a9617deff471
Mektro <mowz971@gmail.com>
2019-06-18 16:12:03 +0100

    Will use Setup comp for picking the F(X) buttons later

--- src/components/setup.js
+++ src/components/setup.js
@@ -1,45 +1,14 @@
 import React, { useState } from "react";
 import "./../global.css";
 
-const PickName = () => {
-  const [username, setUsername] = useState("");
-
-  const newUser = newName => {
-    setUsername(newName);
-  };
-
-  const changeUsername = e => {
-    e.preventDefault();
-    console.log({ username });
-    //Add to localStorage??
-    setUsername("");
-  };
-
-  return (
-    <div>
-      <h3>What would you like to be called?</h3>
-      <form onSubmit={changeUsername}>
-        <input value={username} onChange={e => newUser(e.target.value)} />
-        <button>Next</button>
-      </form>
-
-      <br />
-      <br />
-      <button>Start as Guest</button>
-    </div>
-  );
-};
-
-/*
-Future feature to be able to choose which buttons the user would like to take to the challenge
+//Future feature to be able to choose which function the user would like to take to the challenge
 const PickButtons = () => {
   return (
     <div>
-      <h3>Choose your buttons wisely</h3>
+      <h3>Choose your functions wisely F(X)</h3>
     </div>
   );
 };
-*/
 
 export default function Setup() {
   //const [setupStatus, updateSetup] = useState([])
