29504d294f6b5f97e3e44ecbbab631f59789d8ee
Mektro <mowz971@gmail.com>
2019-06-19 18:25:42 +0100

    Basic functionality on StoreContext

---
+++ src/store/StoreContext.js
@@ -0,0 +1,32 @@
+import React, { createContext, useState } from "react";
+
+export const Context = createContext({});
+
+export const Provider = props => {
+  // Initial values are obtained from the props
+  const {
+    username: initialUsername,
+    onboarded: initialBoarded,
+    children
+  } = props;
+
+  const [username, changeUsername] = useState(initialUsername);
+  const [onboarded, setOnboarded] = useState(initialBoarded);
+
+  // Make the context object:
+  const usersContext = {
+    username,
+    changeUsername,
+    onboarded,
+    setOnboarded
+  };
+  // pass the value in provider and return
+  return <Context.Provider value={usersContext}>{children}</Context.Provider>;
+};
+
+export const { Consumer } = Context;
+
+Provider.defaultProps = {
+  username: "asd",
+  onboarded: false
+};
