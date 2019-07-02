bbc1e0f9d5f388d2e06578ad38958a3cdaf4bf5d
Mektro <mowz971@gmail.com>
2019-07-02 23:42:32 +0100

    Added ResetStorage comp to Settings Page

--- src/Pages/Settings.js
+++ src/Pages/Settings.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ResetStatus from "../components/ResetStatus";
 
 function Settings() {
   return (
     <div>
       <h4>What would you like to change?</h4>
+      <ResetStatus />
     </div>
   );
 }
